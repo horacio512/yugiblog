278122c94b1d1854b632e887bc58f51a586de131
horacio512 <horaciioo@hotmail.com>
2024-03-25 17:56:18 -0300

    adding google maps

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,31 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from '@emotion/react'
 import Tema from '../assets/Tema'
-import { Grid, Link, Typography } from '@mui/material'
+import { Drawer, Grid, Link, Typography } from '@mui/material'
 import stores from "../info.json"
 
 /*icons*/
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 
-const Detail = ({ storeName }) => {
+const Detail = ({ storeName, openNew }) => {
+
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        setOpen(true)
+    }, [])
 
     return (
         <ThemeProvider theme={Tema}>
-                
-            <Grid container display="flex" justifyContent="center">
 
-                <Grid item xs={12} sm={10} mt={3}>
-                    <Typography variant="h2" fontWeight={400} textAlign="center" color="primary">Organiza: {stores[storeName].store} </Typography>
+
+            <Drawer PaperProps={{ sx: { height: '80vh', top: "11%", right: "0%", width: "100%", backgroundColor: "#272525" } }}
+                anchor="right"
+                open={open}
+                onClose={() => { setOpen(false) }}
+            >
+
+                <Grid container display="flex" justifyContent="center">
+
+                    <Grid item xs={11} sm={10} mt={3} textAlign="center">
+                        <Typography variant="h2" fontWeight={400} color="primary">Organiza: {stores[storeName].store} </Typography>
+                        <Typography variant='h2' mt={1} fontWeight={300} color="primary">Ubicación: {stores[storeName].dir}</Typography>
+                    </Grid>
+                    <Grid item xs={10} sm={6} mt={2}>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.05333721711!2d-56.20572772533256!3d-34.905110973476056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f802a05a31c03%3A0x463b367338bd0fc8!2s25%20de%20Mayo%20626%2C%2011000%20Montevideo%2C%20Departamento%20de%20Montevideo!5e0!3m2!1ses-419!2suy!4v1711399638538!5m2!1ses-419!2suy"
+                            width="100%" height="auto" frameborder="0"></iframe>
+                        <Typography variant='h4' mt={1} fontWeight={300} color="primary">{stores[storeName].dir}</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={10} display="flex" justifyContent="center" alignItems="center" mt={2} >
+                        <Typography variant="h4" color="primary">Redes: </Typography>
+                        <Typography color="primary" display="flex">
+                            <Link target="_blank" href={stores[storeName].insta} m={1} ><InstagramIcon fontSize='large' sx={{ color: "#C13584" }} /></Link>
+                            <Link target="_blank" href={stores[storeName].face} m={1}><FacebookIcon fontSize='large' sx={{ color: "#4267B2" }} /></Link>
+                        </Typography>
+
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={10} display="flex" justifyContent="center" alignItems="center" mt={2} >
-                    <Typography variant="h4" color="primary">Redes: </Typography>
-                    <Typography color="primary" display="flex">
-                        <Link target="_blank" href={stores[storeName].insta} m={1} ><InstagramIcon fontSize='large' sx={{ color: "#C13584" }} /></Link>
-                        <Link target="_blank" href={stores[storeName].face} m={1}><FacebookIcon fontSize='large' sx={{ color: "#4267B2" }} /></Link>
-                    </Typography>
-                </Grid>
-            </Grid>
+            </Drawer>
 
         </ThemeProvider >
     )
--- src/pages/Tournament.jsx
+++ src/pages/Tournament.jsx
@@ -14,8 +14,8 @@ import Detail from '../components/Detail';
 
 const Tournament = () => {
 
-    const torneos = [{ id: "kingdom", name: "Kingdom Championship Series", info: "25 de mayo 626", back: all2, front: yugi }, { id: "laspiedras", name: "Liga Fenix las Piedras", info: "Las piedras", back: all, front: kaiba },
-    { id: "lorien", name: "CDS Lorien TCG Club", info: "Colonia 2235", back: all3, front: pegasus }, { id: "geartown", name: "Liga Maldonado Geartown", info: "Gral. Leonardo Olivera 1132", back: all4, front: joey },]
+    const torneos = [{ id: "kingdom", name: "Kingdom Championship Series", back: all2, front: yugi }, { id: "laspiedras", name: "Liga Fenix las Piedras", back: all, front: kaiba },
+    { id: "lorien", name: "CDS Lorien TCG Club", back: all3, front: pegasus }, { id: "geartown", name: "Liga Maldonado Geartown", back: all4, front: joey },]
 
     const [show, setShow] = useState("")
 
@@ -41,10 +41,13 @@ const Tournament = () => {
                                     backgroundImage: `url(${info.back})`, backgroundSize: "cover", backgroundRepeat: "no-repeat",
                                     backgroundPosition: "right", cursor: "pointer", "&:hover": { sm: { transform: "scale(1.1)" } }
                                 }} height="20vh">
-                                <Box sx={{ backdropFilter: "blur(6px)", clipPath: "polygon(0% 0, 100% 0, 70% 100%, 0% 100%)", width: "60%", height: "100%" }}>
-                                    <Grid item xs={12} sm={12} display="flex" flexDirection="column" alignItems="center">
-                                        <Typography variant="h3" textAlign="center" textTransform="capitalize" fontWeight={600} color="primary">{info.name}</Typography>
-                                        <Typography variant="h5" mb={4} textAlign="center" color="primary">{info.info}</Typography>
+                                <Box sx={{ backdropFilter: "blur(6px)", clipPath: "polygon(0% 0, 100% 0, 80% 100%, 0% 100%)", width: "70%", height: "100%" }}>
+                                    <Grid item xs={12} display="flex" flexDirection="column" alignItems="center">
+
+                                        <Grid item xs={6}>
+                                            <Typography variant="h3" textAlign="center" textTransform="capitalize"
+                                                fontWeight={600} color="primary">{info.name}</Typography>
+                                        </Grid>
                                         <TouchAppIcon color="primary" sx={{
                                             display: { xs: "flex", md: "none", opacity: 0.7 }
                                         }} />
@@ -57,7 +60,7 @@ const Tournament = () => {
                                 </Box>
 
                             </Box>
-                            {show === info.id ? (<Detail storeName={info.id} sx={{transform: "rotate(30deg)"}} />) : (<></>)}
+                            {show === info.id ? (<Detail storeName={info.id} />) : (<></>)}
                         </Box>
 
                     ))}
