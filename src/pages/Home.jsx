59dff28e84d7d0140f3a47ddc673fdcfe56f8b97
horacio512 <horaciioo@hotmail.com>
2024-04-05 09:33:01 -0300

    work push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,20 @@ const Home = () => {
             <Grid container display="flex" justifyContent="center" alignItems="space-between" mt={4} mb={15}>
 
                 <Grid item xs={11} sm={10} mb={3}>
-                    <Typography variant="h1" fontWeight={600} textAlign="center" color="primary">Bienvenido Duelista!</Typography>
+                    <Typography variant="h1" fontWeight={400} textAlign="center" color="primary">Bienvenido Duelista!</Typography>
                 </Grid>
 
-                <Grid item xs={11} sm={5}>
-                    <img src={regional} width="100%" height="auto" />
+                <Grid item xs={12} textAlign="center">
+                    <Typography variant="h1" fontWeight={600} color="primary">GANADOR DEL</Typography>
                 </Grid>
+
+                <Grid item xs={11} sm={5} textAlign="center">
+                    <img src={regional} width="80%" height="auto" />
+                </Grid>
+
+                <Button>
+                    Ganador
+                </Button>
 
                 <Grid item xs={11} sm={10}>
 
